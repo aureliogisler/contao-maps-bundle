35655595fd3c34d414432536e9c5326296696925
xippoGmbH <aurelio.gisler@xippo.ch>
2020-11-22 16:42:56 +0100

    Update tl_content.php

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -1,6 +1,5 @@
 // contao/dca/tl_content.php
 $GLOBALS['TL_DCA']['tl_content']['config']['ptable'] = 'tl_xippo_maps_marker';
-$GLOBALS['TL_DCA']['tl_content']['config']['ctable'] = ['tl_xippo_maps'];
 $GLOBALS['TL_DCA']['tl_content']['fields']['xippo_maps'] = [
 			'label' => &$GLOBALS['TL_LANG']['tl_content']['xippo_maps'],
 			'inputType' => 'select',
