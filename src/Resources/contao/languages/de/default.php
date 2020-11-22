e4934e559ce0ec5452e469ebdb3f37f9d472ad69
xippoGmbH <aurelio.gisler@xippo.ch>
2020-11-22 17:06:22 +0100

    Update default.php

--- src/Resources/contao/languages/de/default.php
+++ src/Resources/contao/languages/de/default.php
@@ -15,14 +15,4 @@
  * Content element
  */
 $GLOBALS['TL_LANG']['CTE']['xippo_elements'] = 'Xippo GmbH - Module';
-$GLOBALS['TL_LANG']['CTE']['maps_id'] = ['Karte', 'Eine Karte zum Artikel hinzufügen'];
-
-/**
- * Miscelaneous
- */
-//$GLOBALS['TL_LANG']['MSC'][''] = '';
-
-/**
- * Errors
- */
-//$GLOBALS['TL_LANG']['ERR'][''] = '';
+$GLOBALS['TL_LANG']['CTE']['mapsId'] = ['Karte', 'Eine Karte zum Artikel hinzufügen'];
