4e52ca0514ccf56ee62c904c0bb792757d81afcf
xippoGmbH <aurelio.gisler@xippo.ch>
2020-11-28 19:43:42 +0100

    Update tl_content.php

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -12,15 +12,15 @@
  *
  */
 
-$GLOBALS['TL_DCA']['tl_content']['config']['ptable'] = 'tl_xippo_maps_marker';
-// $GLOBALS['TL_DCA']['tl_content']['fields']['content_maps'] = [
-			// 'label' => &$GLOBALS['TL_LANG']['tl_content']['content_maps'],
-			// 'inputType' => 'select',
-			// 'foreignKey' => 'tl_xippo_maps.title',
-			// 'sql' => ['type' => 'integer', 'unsigned' => true, 'notnull' => true, 'default' => 0],
-			// 'eval' => [
-				// 'mandatory' => true,
-				// 'includeBlankOption' => true
-			// ]
-		// ];
-// $GLOBALS['TL_DCA']['tl_content']['palettes']['xippo_maps'] = '{type_legend},type,headline;{maps_legend},content_maps;{protected_legend:hide},protected;{expert_legend:hide},guests,invisible,cssID,space;';
+//$GLOBALS['TL_DCA']['tl_content']['config']['ptable'] = 'tl_xippo_maps_marker';
+$GLOBALS['TL_DCA']['tl_content']['fields']['content_maps'] = [
+			'label' => &$GLOBALS['TL_LANG']['tl_content']['content_maps'],
+			'inputType' => 'select',
+			'foreignKey' => 'tl_xippo_maps.title',
+			'sql' => ['type' => 'integer', 'unsigned' => true, 'notnull' => true, 'default' => 0],
+			'eval' => [
+				'mandatory' => true,
+				'includeBlankOption' => true
+			]
+		];
+$GLOBALS['TL_DCA']['tl_content']['palettes']['xippo_maps'] = '{type_legend},type,headline;{maps_legend},content_maps;{protected_legend:hide},protected;{expert_legend:hide},guests,invisible,cssID,space;';
